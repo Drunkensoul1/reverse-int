a8a6e082cddb1f8d356768f069e1e75b556e09a6
Egor Lifianets <lifenetsegor@gmail.com>
2021-03-12 01:33:54 +0300

    Try to working cicle, but it's doesn't works

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function reverse(num) {
-   let number = String(num);
-   let j = '';
+    let dig = Math.abs(num);  
+    let number = String(dig);
+    let j = '';
    for (i=0; i < number.length; i++) {
        j = `${number[i]}${j}`;
+
+       if ( num < 0 && i == number.length ) {
+        return Number(-j); 
+       }     
+      
    }
-   return Number(j);
+   return Number(j);  
  }
-console.log(reverse(321));
+
+console.log(reverse(-321122));
