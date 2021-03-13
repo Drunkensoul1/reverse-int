6180fb5adf6b51be630a0bec20116e1aecfb51b4
Egor Lifianets <lifenetsegor@gmail.com>
2021-03-13 13:01:19 +0300

    fixed

--- index.js
+++ index.js
@@ -3,14 +3,7 @@ function reverse(num) {
     let number = String(dig);
     let j = '';
    for (i=0; i < number.length; i++) {
-       j = `${number[i]}${j}`;
-
-       if ( num < 0 && i == number.length ) {
-        return Number(-j); 
-       }     
-      
-   }
-   return Number(j);  
- }
-
-console.log(reverse(-321122));
+       j = `${number[i]}${j}`;  
+}
+return j; 
+}
